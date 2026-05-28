44584e0ff99b0505cf1434953fe9e115fc3f4319
faisalprady13 <faisalprady@gmail.com>
2026-05-28 10:50:51 +0200

    initial commit

---
+++ src/main/java/org/myspring/springrecap/SpringRecapApplication.java
@@ -0,0 +1,13 @@
+package org.myspring.springrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myspring/springrecap/SpringRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myspring.springrecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
